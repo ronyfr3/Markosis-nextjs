3c47233c0f29fa23dde41fcc621a919ad5e1431f
ronyfr3 <babamarony@gmail.com>
2022-03-06 13:01:36 +0600

    resize close button

--- components/Calendly.js
+++ components/Calendly.js
@@ -5,7 +5,7 @@ const Calendly = ({ setShowCalendly }) => {
     <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-50">
       <InlineWidget url="https://calendly.com/rakib-fstackdev/15min" />
       <span
-        className="fixed top-[50px] bg-black text-white px-3 py-1 text-[20px] cursor-pointer right-[-55px] rounded-xl"
+        className="fixed top-[50px] bg-black text-white px-2 border-1 border border-white py-1 text-[20px] cursor-pointer right-[-35px] rounded-sm"
         onClick={() => setShowCalendly(false)}
       >
         x
